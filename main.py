6d279896d104a650adaf17532e58d722d96fbf3e
JyothisreeSajan <jyothisreesajan@gmail.com>
2025-01-30 17:51:08 +0530

    detectface

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import mediapipe as mp
 img_path = "images.jpeg"
 
 img = cv2.imread(img_path)
-
+H,W,_ = img.shape
 #detect images
 mp_face_detection  = mp.solutions.face_detection
 with mp_face_detection.FaceDetection(model_selection=0,min_detection_confidence=0.5) as face_detection:
@@ -19,4 +19,15 @@ with mp_face_detection.FaceDetection(model_selection=0,min_detection_confidence=
             location_data = detection.location_data
             bbox = location_data.relative_bounding_box
             x1,y1,w,h = bbox.xmin , bbox.ymin, bbox.width, bbox.height
+
+            x1 = int(x1 * W)
+            y1 = int(y1 * H)
+            w = int(w * W)
+            h = int(h * H)
+
+            img = cv2.rectangle(img ,(x1,y1) ,(x1+w, y1+h) ,(0,255,0),10)
+    
+    cv2.imshow("image",img)
+    cv2.waitKey(0)
+     
             
